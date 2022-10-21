7828c02eb9e89ca0448eaa6a4507592a6224b3ea
Edi Modrić <edi.modric@gmail.com>
2022-10-21 14:47:28 +0200

    Override value object providers for content and location parameter types to make use of load service

---
+++ lib/Parameters/ValueObjectProvider/ContentProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Ez\SiteApi\Parameters\ValueObjectProvider;
+
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\Repository;
+use Netgen\EzPlatformSiteApi\API\LoadService;
+use Netgen\EzPlatformSiteApi\API\Values\Content;
+use Netgen\Layouts\Parameters\ValueObjectProviderInterface;
+
+final class ContentProvider implements ValueObjectProviderInterface
+{
+    private Repository $repository;
+
+    private LoadService $loadService;
+
+    public function __construct(Repository $repository, LoadService $loadService)
+    {
+        $this->repository = $repository;
+        $this->loadService = $loadService;
+    }
+
+    public function getValueObject($value): ?object
+    {
+        try {
+            /** @var \Netgen\EzPlatformSiteApi\API\Values\Content $content */
+            return $this->repository->sudo(
+                fn (Repository $repository): Content => $this->loadService->loadContent((string) $value),
+            );
+        } catch (NotFoundException $e) {
+            return null;
+        }
+    }
+}
---
+++ lib/Parameters/ValueObjectProvider/LocationProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Ez\SiteApi\Parameters\ValueObjectProvider;
+
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\Repository;
+use Netgen\EzPlatformSiteApi\API\LoadService;
+use Netgen\EzPlatformSiteApi\API\Values\Location;
+use Netgen\Layouts\Parameters\ValueObjectProviderInterface;
+
+final class LocationProvider implements ValueObjectProviderInterface
+{
+    private Repository $repository;
+
+    private LoadService $loadService;
+
+    public function __construct(Repository $repository, LoadService $loadService)
+    {
+        $this->repository = $repository;
+        $this->loadService = $loadService;
+    }
+
+    public function getValueObject($value): ?object
+    {
+        try {
+            /** @var \Netgen\EzPlatformSiteApi\API\Values\Location $location */
+            return $this->repository->sudo(
+                fn (Repository $repository): Location => $this->loadService->loadLocation((string) $value),
+            );
+        } catch (NotFoundException $e) {
+            return null;
+        }
+    }
+}
